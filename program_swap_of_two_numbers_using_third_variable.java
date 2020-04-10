ae43cd0fad06aeff3570f4de69efec34b3359ae8
tarunslathia68 <45391228+tarunslathia68@users.noreply.github.com>
2020-04-10 21:15:29 +0530

    Add files via upload

---
+++ program_swap_of_two_numbers_using_third_variable.java
@@ -0,0 +1,32 @@
+/* W.A.P in JAVA TO SWAP TWO NUMBERS USING THE THIRD VARIABLE*/
+
+public class Swap
+
+{
+
+	public static void main(String args[])
+
+	{
+
+	 
+
+		int x=20;
+
+		int y=10;
+
+		int temp ;
+
+		System.out.print("Before swapping :" + " x ="+ x +",y=" + y);
+
+	    temp = x;
+
+	    x =y ;
+
+	    y = temp;
+
+		System.out.println("\n Values after swapping :" + " x ="+ x +",y=" + y);
+
+	}
+
+}
+
